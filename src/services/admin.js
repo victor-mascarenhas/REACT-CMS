4015fdfa43ae1d2e9a8d611449491037762b8b20
Victor Mascarenhas Reis da Silva <victor.mascarenhas@al.infnet.edu.br>
2020-10-25 17:52:40 -0300

    Creating categories form

--- src/services/admin.js
+++ src/services/admin.js
@@ -7,8 +7,12 @@ const getCategories = () => http.get('/category')
 //Delete
 const delCategories = (_id) => http.delete(`/category/${_id}`)
 
+//Post
+const postCategories = (data) => http.post('/category', data)
+
 export {
     getProducts,
     getCategories,
-    delCategories
+    delCategories,
+    postCategories
 }
